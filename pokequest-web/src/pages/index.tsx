148afafc3d0509963bf67bffdf4fc11830f3815e
Amos <alphatrl1997@gmail.com>
2021-04-05 23:13:54 +0800

    refactor: improve login screen design

--- pokequest-web/src/pages/index.tsx
+++ pokequest-web/src/pages/index.tsx
@@ -2,9 +2,62 @@ import { useRouter } from 'next/router';
 import React, { useCallback } from 'react';
 import { GoogleLogin, GoogleLoginResponse } from 'react-google-login';
 import LoginLayout from '../components/layouts/LoginLayout';
+import styled from 'styled-components';
 import SEO from '../components/SEO';
 
 import { useAppContext } from '../context/state';
+
+const BackgroundImageWrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-image: url('/images/background.png');
+  background-size: cover;
+`;
+
+const ModalWrapper = styled.div`
+  position: fixed;
+  background: #fafafa;
+  width: 40%;
+  min-width: 350px;
+  height: auto;
+  height: 40%;
+  max-height: 600px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 32px;
+  border-radius: 8px;
+  box-shadow: 0px 2px #00000027;
+
+  @media only screen and (max-width: 600px) {
+    width: 90%;
+    padding: 32px 12px;
+
+    h1 {
+      font-size: 1.6rem;
+    }
+  }
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  .instructions {
+    width: 100%;
+
+    h1 {
+      font-size: 3.4rem;
+      margin: 0;
+
+      padding-bottom: 16px;
+    }
+
+    h3 {
+      font-weight: 400;
+      margin: 0;
+    }
+  }
+`;
 
 // Show Login Screen
 const Login: React.FC = () => {
@@ -26,13 +79,23 @@ const Login: React.FC = () => {
   return (
     <LoginLayout>
       <SEO title="Login" />
-      <GoogleLogin
-        clientId={`${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}`}
-        buttonText="Login with Google"
-        onSuccess={handleGoogleSuccess}
-        onFailure={responseGoogle}
-        isSignedIn
-      />
+      <BackgroundImageWrapper>
+        <ModalWrapper>
+          <div className="instructions">
+            <h1>PokeQuest</h1>
+            <h3>Welcome!</h3>
+            <h3>Professor Oak needs your help!</h3>
+          </div>
+
+          <GoogleLogin
+            clientId={`${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}`}
+            buttonText="Login with Google"
+            onSuccess={handleGoogleSuccess}
+            onFailure={responseGoogle}
+            isSignedIn
+          />
+        </ModalWrapper>
+      </BackgroundImageWrapper>
     </LoginLayout>
   );
 };
