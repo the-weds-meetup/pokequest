5245b69365b16993b14c65ed55a5d75e7c58d2c6
Amos <alphatrl1997@gmail.com>
2021-04-09 23:22:47 +0800

    refactor: update dockerignore to ignore env

---
+++ pokequest-web/next.config.js
@@ -0,0 +1,20 @@
+console.log(process.env.GOOGLE_CLIENT_ID);
+
+module.exports = {
+  target: 'serverless',
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack', 'url-loader'],
+    });
+
+    config.node.fs = 'empty';
+
+    return config;
+  },
+
+  env: {
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  },
+};
